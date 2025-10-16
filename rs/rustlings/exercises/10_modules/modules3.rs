222be3d17c8f288e1ec340ec2487b49f9778fd26
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2025-10-16 14:46:06 +0000

    feat: update rustlings exercises

--- rs/rustlings/exercises/10_modules/modules3.rs
+++ rs/rustlings/exercises/10_modules/modules3.rs
@@ -5,6 +5,8 @@
 // your scope. Bonus style points if you can do it with one line!
 // use ???;
 
+use std::time::{SystemTime, UNIX_EPOCH};
+
 fn main() {
     match SystemTime::now().duration_since(UNIX_EPOCH) {
         Ok(n) => println!("1970-01-01 00:00:00 UTC was {} seconds ago!", n.as_secs()),
--- rs/rustlings/exercises/11_hashmaps/hashmaps3.rs
+++ rs/rustlings/exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,13 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        let team_1 = scores.entry(team_1_name).or_insert(TeamScores::default());
+        team_1.goals_scored += team_1_score;
+        team_1.goals_conceded += team_2_score;
+
+        let team_2 = scores.entry(team_2_name).or_insert(TeamScores::default());
+        team_2.goals_scored += team_2_score;
+        team_2.goals_conceded += team_1_score;
     }
 
     scores
@@ -54,9 +61,11 @@ England,Spain,1,0";
     fn build_scores() {
         let scores = build_scores_table(RESULTS);
 
-        assert!(["England", "France", "Germany", "Italy", "Poland", "Spain"]
-            .into_iter()
-            .all(|team_name| scores.contains_key(team_name)));
+        assert!(
+            ["England", "France", "Germany", "Italy", "Poland", "Spain"]
+                .into_iter()
+                .all(|team_name| scores.contains_key(team_name))
+        );
     }
 
     #[test]
