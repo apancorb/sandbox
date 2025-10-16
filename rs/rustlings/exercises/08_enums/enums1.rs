237e329365d90de9a63c2260b17e7f49b2f359f0
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2025-10-16 14:04:57 +0000

    feat: update rustlings exercises

---
+++ rs/rustlings/exercises/08_enums/enums1.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+enum Message {
+    Resize,
+    Move,
+    Echo,
+    ChangeColor,
+    Quit
+    // TODO: Define a few types of messages as used below.
+}
+
+fn main() {
+    println!("{:?}", Message::Resize);
+    println!("{:?}", Message::Move);
+    println!("{:?}", Message::Echo);
+    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Quit);
+}
