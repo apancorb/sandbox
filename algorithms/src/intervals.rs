591d697397eaccce981bed16e6668c41d3c1cd0e
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2025-12-14 10:37:08 +0000

    feat: add impl for identify all interval overlaps

--- algorithms/src/intervals.rs
+++ algorithms/src/intervals.rs
@@ -36,9 +36,103 @@ pub fn merge_intervals(intervals: &mut [[i32; 2]]) -> Vec<[i32; 2]> {
     merged
 }
 
+/// Identify All Interval Overlaps
+///
+/// Return an array of all overlaps between two arrays of intervals: intervals1 and intervals2.
+/// Each individual interval array is sorted by start value, and contains no overlapping
+/// intervals within itself.
+///
+/// # Example
+///
+/// ```text
+/// Input: intervals1 = [[1, 4], [5, 6], [9, 10]], intervals2 = [[2, 7], [8, 9]]
+/// Output: [[2, 4], [5, 6], [9, 9]]
+/// ```
+///
+/// # Constraints
+///
+/// - For every index i in intervals1, intervals1[i].start <= intervals1[i].end.
+/// - For every index j in intervals2, intervals2[j].start <= intervals2[j].end.
+pub fn find_overlaps(intervals1: &[[i32; 2]], intervals2: &[[i32; 2]]) -> Vec<[i32; 2]> {
+    let mut i = 0;
+    let mut j = 0;
+    let mut res = Vec::new();
+
+    while i < intervals1.len() && j < intervals2.len() {
+        let a;
+        let b;
+        if intervals1[i][0] <= intervals2[j][0] {
+            a = intervals1[i];
+            b = intervals2[j];
+        } else {
+            a = intervals2[j];
+            b = intervals1[i];
+        }
+
+        if a[1] >= b[0] {
+            res.push([b[0], a[1].min(b[1])]);
+        }
+
+        if intervals1[i][1] <= intervals2[j][1] {
+            i += 1;
+        } else {
+            j += 1;
+        }
+    }
+
+    res
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_find_overlaps_example() {
+        assert_eq!(
+            find_overlaps(&[[1, 4], [5, 6], [9, 10]], &[[2, 7], [8, 9]]),
+            vec![[2, 4], [5, 6], [9, 9]]
+        );
+    }
+
+    #[test]
+    fn test_find_overlaps_no_overlap() {
+        assert_eq!(
+            find_overlaps(&[[1, 2], [5, 6]], &[[3, 4], [7, 8]]),
+            Vec::<[i32; 2]>::new()
+        );
+    }
+
+    #[test]
+    fn test_find_overlaps_full_overlap() {
+        assert_eq!(
+            find_overlaps(&[[1, 10]], &[[2, 4], [6, 8]]),
+            vec![[2, 4], [6, 8]]
+        );
+    }
+
+    #[test]
+    fn test_find_overlaps_single_point() {
+        assert_eq!(find_overlaps(&[[1, 3]], &[[3, 5]]), vec![[3, 3]]);
+    }
+
+    #[test]
+    fn test_find_overlaps_empty_first() {
+        assert_eq!(find_overlaps(&[], &[[1, 2]]), Vec::<[i32; 2]>::new());
+    }
+
+    #[test]
+    fn test_find_overlaps_empty_second() {
+        assert_eq!(find_overlaps(&[[1, 2]], &[]), Vec::<[i32; 2]>::new());
+    }
+
+    #[test]
+    fn test_find_overlaps_multiple() {
+        assert_eq!(
+            find_overlaps(&[[0, 2], [5, 10], [13, 23]], &[[1, 5], [8, 12], [15, 20]]),
+            vec![[1, 2], [5, 5], [8, 10], [15, 20]]
+        );
+    }
 
     #[test]
     fn test_merge_intervals_example() {
