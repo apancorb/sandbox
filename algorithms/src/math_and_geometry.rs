bb35ab22ba1ecbe61a49a2f91f0dce3361e6013b
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2026-01-15 11:11:33 +0000

    feat: add impl for spiral traversal

--- algorithms/src/math_and_geometry.rs
+++ algorithms/src/math_and_geometry.rs
@@ -1,4 +1,100 @@
+/// Spiral Traversal
+///
+/// Return the elements of a matrix in clockwise spiral order.
+///
+/// # Example
+///
+/// ```text
+/// Input:
+///   [[ 0,  1,  2,  3,  4],
+///    [ 5,  6,  7,  8,  9],
+///    [10, 11, 12, 13, 14],
+///    [15, 16, 17, 18, 19]]
+///
+/// Output: [0, 1, 2, 3, 4, 9, 14, 19, 18, 17, 16, 15, 10, 5, 6, 7, 8, 13, 12, 11]
+/// ```
+pub fn spiral_traversal(matrix: &[Vec<i32>]) -> Vec<i32> {
+    if matrix.is_empty() {
+        return Vec::new();
+    }
+
+    let mut result = Vec::new();
+
+    let mut top = 0;
+    let mut bottom = matrix.len() - 1;
+    let mut left = 0;
+    let mut right = matrix[0].len() - 1;
+
+    while top <= bottom && left <= right {
+        // Move from left to right along the top boundry
+        for i in left..=right {
+            result.push(matrix[top][i]);
+        }
+        top += 1;
+
+        // Move from top to bottom along the right boundry
+        for i in top..=bottom {
+            result.push(matrix[i][right]);
+        }
+        right -= 1;
+
+        // Check that the bottom boundary hasn't passed the top boundary
+        // before moving from right to left along the bottom boundary.
+        if top <= bottom {
+            for i in (left..=right).rev() {
+                result.push(matrix[bottom][i]);
+            }
+            bottom -= 1;
+        }
+
+        // Check that the left boundary hasn't passed the right boundary
+        // before moving from bottom to top along the left boundary.
+        if left <= right {
+            for i in (top..=bottom).rev() {
+                result.push(matrix[i][left]);
+            }
+            left += 1;
+        }
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_spiral_traversal_example() {
+        let matrix = vec![
+            vec![0, 1, 2, 3, 4],
+            vec![5, 6, 7, 8, 9],
+            vec![10, 11, 12, 13, 14],
+            vec![15, 16, 17, 18, 19],
+        ];
+        assert_eq!(
+            spiral_traversal(&matrix),
+            vec![
+                0, 1, 2, 3, 4, 9, 14, 19, 18, 17, 16, 15, 10, 5, 6, 7, 8, 13, 12, 11
+            ]
+        );
+    }
+
+    #[test]
+    fn test_spiral_traversal_single_row() {
+        let matrix = vec![vec![1, 2, 3, 4]];
+        assert_eq!(spiral_traversal(&matrix), vec![1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn test_spiral_traversal_square() {
+        let matrix = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        assert_eq!(spiral_traversal(&matrix), vec![1, 2, 3, 6, 9, 8, 7, 4, 5]);
+    }
+
+    #[test]
+    fn test_spiral_traversal_empty() {
+        let matrix: Vec<Vec<i32>> = vec![];
+        assert_eq!(spiral_traversal(&matrix), vec![]);
+    }
 }
